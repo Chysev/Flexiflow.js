07c3462899adfd5f1e132e54ccf115014162cebf
Chysev <Johnlayda92@gmail.com>
2023-05-13 19:47:08 +0400

    renamed the app.tsx to index.ts

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+
+import React from "react"
+import Flexiflow from "../../public/Flexiflow.png"
+
+const Home = () => {
+    return (
+        <div>
+            <img src={Flexiflow} className="ExReact" />
+        </div>
+    )
+}
+
+export default Home
